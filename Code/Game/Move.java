8863e542d2334c76514a2ff478a0719aaf17a25a
Christos Gkovaris <christosgkovariscs52@gmail.com>
2024-11-08 19:50:31 +0200

    Add files via upload

---
+++ Code/Game/Move.java
@@ -0,0 +1,13 @@
+public class Move {
+    int row;
+    int col;
+    char letter;
+
+    
+    // Constructor for a move
+    public Move(int row, int col, char letter) {
+        this.row = row;
+        this.col = col;
+        this.letter = letter;
+    }
+}
