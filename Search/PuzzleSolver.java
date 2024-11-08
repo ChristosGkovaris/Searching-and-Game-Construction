7e7112d16aeba48a4179f4c823ede416b52d1add
Christos Gkovaris <christosgkovariscs52@gmail.com>
2024-11-08 19:44:57 +0200

    Update PuzzleSolver.java

--- Search/PuzzleSolver.java
+++ Search/PuzzleSolver.java
@@ -5,11 +5,6 @@ class PuzzleSolver {
     // The goal state of the puzzle
     private int[][] goalState = {{6, 5, 4}, {7, 0, 3}, {8, 1, 2}};
 
-    /**
-     * Solves the puzzle.
-     * @param initialState The initial state of the puzzle.
-     * @param useAStar Flag indicating whether to use A* algorithm.
-     */
     public void solve(int[][] initialState, boolean useAStar) {
         
         // Check if the initial state is valid
@@ -21,7 +16,8 @@ class PuzzleSolver {
         // Priority queue for frontier states
         PriorityQueue<PuzzleState> frontier = new PriorityQueue<>(Comparator.comparingInt(s -> (useAStar ? s.getCost() + s.misplacedTilesHeuristic(goalState) : s.getCost())));
         Set<PuzzleState> explored = new HashSet<>();
-        Set<PuzzleState> inFrontier = new HashSet<>(); // This set will mirror the frontier for quick lookup
+        // This set will mirror the frontier for quick lookup
+        Set<PuzzleState> inFrontier = new HashSet<>(); 
         
         // Create root state and add to frontier
         PuzzleState root = new PuzzleState(initialState, findZeroRow(initialState), findZeroCol(initialState), null, 0, 0);
@@ -56,11 +52,6 @@ class PuzzleSolver {
         }
     }
 
-    /**
-     * Checks if the given state is valid.
-     * @param state The state to be validated.
-     * @return True if the state is valid, false otherwise.
-     */
     private boolean isValidState(int[][] state) {
         Set<Integer> tiles = new HashSet<>();
         for (int[] row : state) {
@@ -78,10 +69,6 @@ class PuzzleSolver {
         return true;
     }
 
-    /**
-     * Prints the solution path.
-     * @param state The final state of the solution.
-     */
     private void printSolution(PuzzleState state) {
         List<PuzzleState> path = state.getPathFromStart();
         System.out.println("Path to solution:");
@@ -99,11 +86,6 @@ class PuzzleSolver {
         System.out.println("Depth: " + state.getDepth()); 
     }
 
-    /**
-     * Finds the row index of the zero tile.
-     * @param state The puzzle state.
-     * @return The row index of the zero tile.
-     */
     private int findZeroRow(int[][] state) {
         for (int i = 0; i < state.length; i++) {
             for (int j = 0; j < state[i].length; j++) {
@@ -117,11 +99,6 @@ class PuzzleSolver {
         return -1; 
     }
 
-    /**
-     * Finds the column index of the zero tile.
-     * @param state The puzzle state.
-     * @return The column index of the zero tile.
-     */
     private int findZeroCol(int[][] state) {
         for (int i = 0; i < state.length; i++) {
             for (int j = 0; j < state[i].length; j++) {
